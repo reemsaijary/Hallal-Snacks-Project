037c39ffaaeb615510941b48a523d0b5dce8ab79
Reem Saijary <reemsaijary31@gmail.com>
2025-11-08 16:31:34 +0200

    Built Home page design and styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../assets/logoo.jpeg";
+import "./Navbar.css";
+
+function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => setScrolled(window.scrollY > 50);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <nav className={`navbar navbar-expand-lg fixed-top ${scrolled ? "scrolled" : "transparent"}`}>
+      <div className="container">
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+          <img src={logo} alt="Hallal Snacks Logo" width="80" height="80" className="me-2 rounded-circle"/>
+          <span>Hallal Snacks</span>
+        </Link>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/menu">Menu</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Menu.js
@@ -0,0 +1,4 @@
+function Menu(){
+
+}
+export default Menu;
