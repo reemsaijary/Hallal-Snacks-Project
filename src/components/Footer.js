3ca1eab15207c48aa7886106fdedaacdadbd555b
Reem Saijary <reemsaijary31@gmail.com>
2025-11-09 04:02:32 +0200

    Build footer component design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,13 @@
-function Footer(){
+import React from "react";
 
+function Footer() {
+  return (
+    <footer className="bg-light text-center text-lg-start mt-5">
+      <div className="text-center p-3">
+        © 2025 Hallal Snacks. All rights are reserved.
+      </div>
+    </footer>
+  );
 }
+
 export default Footer;
